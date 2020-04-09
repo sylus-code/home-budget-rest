d0820cdc9e6cd0d9a34af819847f7616d03188b5
cenee <ceneemail@gmail.com>
2020-04-09 15:32:58 +0200

    HB-11 transaction controller

---
+++ src/Controller/Transaction/AddController.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Controller\Transaction;
+
+
+use App\Entity\Transaction;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class AddController
+{
+    public function __construct(EntityManagerInterface $entityManager, SerializerInterface $denormalizer )
+    {
+        $this->denormalizer = $denormalizer;
+        $this->em = $entityManager;
+    }
+
+    /**
+     * @Route( path="/api/transaction", name="transaction_add", methods={"POST"})
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function action(Request $request)
+    {
+        $transactionData = json_decode($request->getContent(), true);
+        /** @var Transaction $transaction */
+        $transaction = $this->denormalizer->denormalize($transactionData, Transaction::class);
+        $transaction->setUserId(1);
+        $this->em->persist($transaction);
+        $this->em->flush();
+
+        return new JsonResponse([], JsonResponse::HTTP_CREATED);
+    }
+}
---
+++ src/Controller/Transaction/GetListController.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App\Controller\Transaction;
+
+
+use App\Repository\TransactionRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class GetListController
+{
+    private $transactionRepository;
+    private $serializer;
+
+    public function __construct( TransactionRepository $transactionRepository, SerializerInterface $serializer)
+    {
+        $this->transactionRepository = $transactionRepository;
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * @Route( path="/api/transaction", name="transaction_get_list", methods={"GET"})
+     * @return JsonResponse
+     */
+    public function action():JsonResponse
+    {
+        $transactions = $this
+            ->transactionRepository
+            ->findAll();
+
+        if (!$transactions)
+        {
+            return new JsonResponse([], 404);
+        }
+        return new JsonResponse($this->serializer->normalize($transactions));
+    }
+}
